1670240d9eacda6179fcf8f9325e82ad2673788a
Krzysztof Grabania <krzysztof.grabania@gmail.com>
2015-10-09 23:03:29 +0200

    Initial v2

--- taxonomy-tweaker.php
+++ taxonomy-tweaker.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Taxonomies Tweaker
+Plugin Name: Taxonomy Tweaker
 Plugin URI: http://grabania.pl/taxonomy-tweaker
 Description: Simply and powerful taxonomy tweaker. Allows change default taxonomy behaviour and add or edit created taxonomies.
 Version: 1.0
@@ -8,35 +8,34 @@ Author: Krzysztof Grabania
 Author URI: http://grabania.pl
 */
 
-$taxonomies_tweaker = new TaxonomiesTweaker();
+$taxonomies_tweaker = new TaxonomyTweaker();
 
-class TaxonomiesTweaker {
+class Taxonomyweaker {
 	public function __construct() {
 		add_action('admin_menu', array($this, 'tt_admin_menu'));
 		add_action('admin_enqueue_scripts', array($this, 'tt_scripts'));
 		add_filter('wp_terms_checklist_args', array($this, 'tt_taxonomy_list_args'));
-		add_action('init', array($this, 'custom_taxonomy'), 0 );
 	}
 	
 	// Menu page
 	public function tt_admin_menu() {
 		add_menu_page(
-			__('Taxonomies Tweaker', 'taxonomies-tweaker'),
-			__('Taxonomies Tweaker', 'taxonomies-tweaker'),
-			'manage_options',
-			'taxonomies-tweaker',
-			array($this, 'tt_options'),
-			'dashicons-hammer',
-			31
+				__('Taxonomies Tweaker', 'taxonomy-tweaker'),
+				__('Taxonomies Tweaker', 'taxonomy-tweaker'),
+				'manage_options',
+				'taxonomy-tweaker',
+				array($this, 'tt_options'),
+				'dashicons-hammer',
+				31
 		);
 		
 		add_submenu_page(
-			'taxonomies-tweaker',
-			__('Add New Taxonomy', 'taxonomies-tweaker'),
-			__('Add New Taxonomy', 'taxonomies-tweaker'),
-			'manage_options',
-			'add-taxonomy',
-			array($this, 'tt_add_taxonomy')
+				'taxonomy-tweaker',
+				__('Add New Taxonomy', 'taxonomy-tweaker'),
+				__('Add New Taxonomy', 'taxonomy-tweaker'),
+				'manage_options',
+				'add-taxonomy',
+				array($this, 'tt_add_taxonomy')
 		);
 	}
 	
@@ -63,39 +62,5 @@ class TaxonomiesTweaker {
 		if ($options['disable_checked_on_top'])
 			$args['checked_ontop'] = false;
 		return $args;
-	}
-	
-	function custom_taxonomy() {
-	
-		$labels = array(
-				'name'                       => _x( 'Taxonomies', 'Taxonomy General Name', 'text_domain' ),
-				'singular_name'              => _x( 'Taxonomy', 'Taxonomy Singular Name', 'text_domain' ),
-				'menu_name'                  => __( 'Taxonomy', 'text_domain' ),
-				'all_items'                  => __( 'All Items', 'text_domain' ),
-				'parent_item'                => __( 'Parent Item', 'text_domain' ),
-				'parent_item_colon'          => __( 'Parent Item:', 'text_domain' ),
-				'new_item_name'              => __( 'New Item Name', 'text_domain' ),
-				'add_new_item'               => __( 'Add New Item', 'text_domain' ),
-				'edit_item'                  => __( 'Edit Item', 'text_domain' ),
-				'update_item'                => __( 'Update Item', 'text_domain' ),
-				'view_item'                  => __( 'View Item', 'text_domain' ),
-				'separate_items_with_commas' => __( 'Separate items with commas', 'text_domain' ),
-				'add_or_remove_items'        => __( 'Add or remove items', 'text_domain' ),
-				'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
-				'popular_items'              => __( 'Popular Items', 'text_domain' ),
-				'search_items'               => __( 'Search Items', 'text_domain' ),
-				'not_found'                  => __( 'Not Found', 'text_domain' ),
-		);
-		$args = array(
-				'labels'                     => $labels,
-				'hierarchical'               => true,
-				'public'                     => true,
-				'show_ui'                    => true,
-				'show_admin_column'          => true,
-				'show_in_nav_menus'          => true,
-				'show_tagcloud'              => true,
-		);
-		register_taxonomy( 'taxonomy', array( 'post' ), $args );
-	
 	}
 }
--- views/admin/option.php
+++ views/admin/option.php
@@ -5,26 +5,28 @@
 		<table class="form-table">
 			<tr>
 				<th><?php _e('Checkboxes sorting', 'taxonomy_tweaker') ?></th>
-				<td>
-					<label for="checked_on_top">
-						<input type="hidden" value="0" name="tt[disable_checked_on_top]" />
-						<input type="checkbox" value="1" name="tt[disable_checked_on_top]"  id="disable_checked_on_top" <?php echo ($options['disable_checked_on_top'] ? 'checked="checked"' : '') ?> />
-						<?php _e('Disable checked taxonomies on top', 'taxonomy_tweaker') ?>
+				<td><label for="checked_on_top"> <input type="hidden" value="0"
+						name="tt[disable_checked_on_top]" /> <input type="checkbox"
+						value="1" name="tt[disable_checked_on_top]"
+						id="disable_checked_on_top"
+						<?php echo ($options['disable_checked_on_top'] ? 'checked="checked"' : '') ?> />
+						<?php _e('Disable checked taxonomies on top', 'taxonomy_tweaker')?>
 					</label>
 					<p class="description"><?php _e('Check if checked taxonomies shouldn\'t be displayed first.', 'taxonomy_tweaker')?></p>
 				</td>
 			</tr>
 			<tr>
 				<th><?php _e('Check children with parent', 'taxonomy_tweaker') ?></th>
-				<td>
-					<label for="children_with_parent">
-						<input type="hidden" value="0" name="tt[children_with_parent]" />
-						<input type="checkbox" value="1" name="tt[children_with_parent]"  id="children_with_parent" <?php echo ($options['children_with_parent'] ? 'checked="checked"' : '') ?> />
-						<?php _e('Check children checkboxes with parent checkbox', 'taxonomy_tweaker') ?>
-					</label>
-				</td>
+				<td><label for="children_with_parent"> <input type="hidden"
+						value="0" name="tt[children_with_parent]" /> <input
+						type="checkbox" value="1" name="tt[children_with_parent]"
+						id="children_with_parent"
+						<?php echo ($options['children_with_parent'] ? 'checked="checked"' : '') ?> />
+						<?php _e('Check children checkboxes with parent checkbox', 'taxonomy_tweaker')?>
+					</label></td>
 			</tr>
 		</table>
-		<?php submit_button() ?>
+		<?php submit_button()?>
 	</form>
-</div> <!-- end .wrap -->
+</div>
+<!-- end .wrap -->
